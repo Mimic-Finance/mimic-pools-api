86fee6c6c39928b5dc0288e8732e64a1ef460fe7
aom wara <waranat.cpe@gmail.com>
2022-05-05 03:19:48 +0700

    feat: keep price to db

--- routes/price.js
+++ routes/price.js
@@ -13,6 +13,9 @@ const INFURA_URL = process.env.INFURA_URL;
 const PROVIDER = new ethers.providers.JsonRpcProvider(INFURA_URL);
 const QUOTER_ADDRESS = "0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6";
 
+const { MongoClient } = require("mongodb");
+const uri = process.env.DB_CONNECT_STRING;
+
 //Define Express App
 const express = require("express");
 const route = express.Router();
@@ -21,13 +24,54 @@ route.get("/", async (req, res) => {
   return res.status(200).json({ message: "Mimic Finance Price API" });
 });
 
-route.get("/:poolAddress", async (req, res) => {
+route.get("/fetch/:poolAddress", async (req, res) => {
   try {
+    const client = new MongoClient(uri);
+
     const price_data = await getPrice(1, req.params.poolAddress);
+    await client.connect();
+    await client.db("price").collection(req.params.poolAddress).insertOne({
+      price: price_data.amountOut,
+      token1: price_data.tokenSymbol0,
+      token2: price_data.tokenSymbol1,
+      updateAt: new Date(),
+    });
+    await client.close();
+
     return res.status(200).json(price_data);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
+});
+
+route.get("/:poolAddress", async (req, res) => {
+  try {
+    const client = new MongoClient(uri);
+    await client.connect();
+    const prices = await client
+      .db("price")
+      .collection(req.params.poolAddress)
+      .find({})
+      .sort({ updateAt: -1 })
+      .toArray();
+
+    await client.close();
+
+    res.status(200).send(prices.length > 0 ? prices[0] : {});
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+route.get("/clear/:poolAddress", async (req, res) => {
+  const client = new MongoClient(uri);
+  await client.connect();
+  const pools = await client
+    .db("price")
+    .collection(req.params.poolAddress)
+    .drop({});
+  await client.close();
+  res.status(200).send(pools);
 });
 
 /*=======================GET PRICE FUNCTION==============================*/
